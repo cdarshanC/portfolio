c1aed028cbf8405c4c18e7208d59a8d4b1f77ab6
cdarshanC <darshanc1996@gmail.com>
2024-06-03 23:03:29 +0530

    Updated projects
    FitWrist and Built for flight mobile wireframe

--- script.js
+++ script.js
@@ -29,6 +29,14 @@ const projects = [
     tags: ["resume"],
   },
   {
+    name: "FitWrist Mobile App",
+    img: "fitwrist-mobile-app.png",
+
+    liveUrl:
+      "https://www.figma.com/proto/vEpTjmGlKaxgANY0k6jD0Q/Smart-watch-E-Commerce-frame-1?node-id=2006-17&t=WAW9jlXi3NbHj2LO-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2006%3A17",
+    tags: ["prototype"],
+  },
+  {
     name: "Logos design list",
     img: "logos-2.png",
 
@@ -45,12 +53,20 @@ const projects = [
     tags: ["uidesign"],
   },
   {
-    name: "Website Mock-ups",
+    name: "Adden AI Website Mock-up",
     img: "adden-ai-website.png",
 
     liveUrl:
-      "https://www.figma.com/design/oqpQ2fzP9N7oeotnsmITgZ/Adden-AI-WEBSITE-Mock-up?node-id=0%3A1&t=86RAY5lKvuBqUzM3-1",
+      "https://www.figma.com/proto/oqpQ2fzP9N7oeotnsmITgZ/Adden-AI-WEBSITE-Mock-up?node-id=1-2&t=FDuGm6bXJJ900AfX-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2",
     tags: ["website"],
+  },
+  {
+    name: "Built For Flight Wireframe",
+    img: "mobile_wireframe_1.png",
+
+    liveUrl:
+      "https://www.figma.com/proto/rISr4PzCjKlIh3oEHYlFs1/Wireframing-assignment-1?node-id=2-2&t=pXAYE4WHPCYVIniV-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A2&show-proto-sidebar=1",
+    tags: ["wireframe"],
   },
 ];
 
